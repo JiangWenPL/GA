cce452e0d32c2a9656cdcc1d9883d2544feb7b88
JiangWenPL <JiangWenPL@users.noreply.github.com>
2017-05-03 10:18:25 +0800

    Creat file.

---
+++ Tribalism.cpp
@@ -0,0 +1,21 @@
+/*
+*    Tribalism.cpp
+*    Cpp Project for 2017 ZJU Mathematical Modeling Contest.
+*	 Team production.
+*	 Team member: Jiang Wen, He Kangrui, Cao Zhefeng.
+*    This file was created by Wen Jiang on May 3, 2017.
+*    Copyright (c) 2017 Wen Jiang. All rights reserved.
+*/
+
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
