7c458f3a7d5d1dc2816d4c160e141d8a808aff2c
JiangWenPL <JiangWenPL@users.noreply.github.com>
2017-05-27 16:14:42 +0800

    To publish

--- 796/796+支撑材料/MinMax.m
+++
@@ -1,7 +0,0 @@
-mnmx=zeros(size(Origin_Data,2),2);
-Normalized_Data=zeros(size(Origin_Data,1),size(Origin_Data,2));
-for i=1:1:5
-    mnmx(i,:)=minmax(Origin_Data(:,i)');
-    Normalized_Data(:,i)=(Origin_Data(:,i)-repmat(mnmx(i,1),size(Origin_Data,1),1))/(mnmx(i,2)-mnmx(i,1));
-end
-save MinMax_Normalized;
